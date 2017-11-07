51e50f3bcc5d1cac473194f128edbec8a4ebb805
Dave Childers <dachilde@cisco.com>
2017-11-07 09:51:09 -0500

    add new als data w/ cleaned names

--- data-raw/als.R
+++ data-raw/als.R
@@ -1,5 +1,6 @@
 library(readr)
-
+library(dplyr)
+library(stringr)
 
 als <- read_delim(
   "https://web.stanford.edu/~hastie/CASI_files/DATA/ALS.txt",
@@ -7,3 +8,14 @@ als <- read_delim(
   )
 
 devtools::use_data(als)
+
+als2 <- als
+
+# How to remove trailing underscores from column names
+# practice regex...
+names(als2) <- tolower(names(als2)) %>%
+  str_replace_all("\\.", "_") %>%
+  str_replace_all("_{2,}", "_") %>%
+  str_replace_all("_$", "")
+
+devtools::use_data(als2)
